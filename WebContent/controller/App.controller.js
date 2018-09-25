3bdd7de5b0d6083763aef8e4826a1c6575824dd2
kiran.rachamalla <kiran.rachamalla@HB-L400410.vistex.com>
2018-09-25 11:41:15 +0530

    Till Step -15 Nested Views

--- WebContent/controller/App.controller.js
+++ WebContent/controller/App.controller.js
@@ -1,21 +1,7 @@
   sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
-], function (Controller , MessageToast ) {
+], function (Controller) {
    "use strict";
-   return Controller.extend("kiran.Myapp.controller.App", {
-      onShowHello : function () {
-         // show a native JavaScript alert
-//         alert("Hello World");
-          // read msg from i18n model
-    	  var oBundle = this.getView().getModel("i18n").getResourceBundle();
-          //var oBundle = this.getView().getModel("ModelName").getResourceBundle();
-          
-          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-          // show message
-          MessageToast.show(sMsg);
-//    	  MessageToast.show("Hello World");
-      }
+   return Controller.extend("kiran.Myapp.controller.App",{
    });
 });
---
+++ WebContent/controller/HelloPanel.controller.js
@@ -0,0 +1,21 @@
+  sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller , MessageToast ) {
+   "use strict";
+   return Controller.extend("kiran.Myapp.controller.HelloPanel", {
+      onShowHello : function () {
+         // show a native JavaScript alert
+//         alert("Hello World");
+          // read msg from i18n model
+    	  var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          //var oBundle = this.getView().getModel("ModelName").getResourceBundle();
+          
+          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+          // show message
+          MessageToast.show(sMsg);
+//    	  MessageToast.show("Hello World");
+      }
+   });
+});
