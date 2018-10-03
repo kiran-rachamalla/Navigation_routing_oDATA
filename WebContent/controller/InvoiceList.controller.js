409274e11fa8378a9399a14e278a349fabab513c
kiran.rachamalla <kiran.rachamalla@HB-L400410.vistex.com>
2018-10-03 15:12:12 +0530

    Step-24 - Filtering

--- WebContent/controller/InvoiceList.controller.js
+++ WebContent/controller/InvoiceList.controller.js
@@ -2,17 +2,33 @@ sap.ui.define([
 		"sap/ui/core/mvc/Controller",
 		"sap/ui/model/json/JSONModel",
 		"kiran/Myapp/model/formatter",
-		"kiran/Myapp/model/ownformatter"
-], function (controller , JSONModel , formatter ,ownformatter){
+		"kiran/Myapp/model/ownformatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator"
+], function (controller , JSONModel , formatter ,ownformatter , Filter , FilterOperator){
 	"use strict"
 	return controller.extend("kiran.Myapp.controller.InvoiceList",{
-		ownformatter: ownformatter,
+		local_icon_parameter: ownformatter,
 		formatter: formatter,
 		onInit: function(){
 			let oData = new JSONModel({
 				currency: "USD"
 			});
 			this.getView().setModel(oData,"view");
+		},
+	
+		onFilterInvoices: function(oEvent){
+			let aFilter = [];
+			let sQuery = oEvent.getParameter('query');
+			
+			if(sQuery){
+				aFilter.push( new Filter("ProductName",FilterOperator.Contains,sQuery) );
+			}
+			
+			let oList = this.byId("invoiceList");
+			let oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+			
 		}
 		
 	});
