51977b2212324a9e2de254746234d0d3194157e4
racha <racha@LAPTOP-5RQR0K35>
2018-10-20 06:59:51 +0530

    With Error

--- WebContent/controller/employee/EmployeeList.controller.js
+++ WebContent/controller/employee/EmployeeList.controller.js
@@ -4,6 +4,10 @@ sap.ui.define([
 	"use strict";
 	
 	return BaseController.extend("kiran.Myapp.controller.employee.EmployeeList",{
-		
+		onListItemPressed : function(oEvent){
+			let oArgs;
+			oArgs = oEvent.getSource().getBindingContext();
+			this.getRouter().navTo("employee",{ employeeId: oArgs.getProperty("EmployeeID") } );
+		}
 	});
 });
