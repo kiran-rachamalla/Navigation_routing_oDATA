b74ef8e684b3cf831f1bf00cd794f7da1916a3e6
racha <racha@LAPTOP-5RQR0K35>
2018-10-23 15:18:22 +0530

    Step 8: Navigate with Flip Transition

--- WebContent/controller/employee/Employee.controller.js
+++ WebContent/controller/employee/Employee.controller.js
@@ -1,45 +1,45 @@
 sap.ui.define([
-	"kiran/Myapp/controller/BaseController"
+"kiran/Myapp/controller/BaseController"
 ], function(BaseController){
-	"use strict";
-	
-	return BaseController.extend("kiran.Myapp.controller.employee.Employee",{
-		onInit: function(){
-			let oRouter = this.getRouter();
-			oRouter.getRoute("employee").attachMatched(this._onRouteMatched,this);
-		},
-	    _onRouteMatched:function(oEvent){
-	    	var oView,oArgs;
-	    	
-	    	oArgs = oEvent.getParameters("arguments");
-	    	oView = this.getView();
-	    	
-	    	oView.bindElement( {
-	    				path: `/Employees(` + oArgs.employeedId + `)` ,
-	    			    event: {
-	    			    	change: this._onBindChange.bind(this),
-	    			    	dataRequested:function(oEvent){
-	    			    		oView.setBusy(true)
-	    			    	},
-	    			    	dataRecivied: function(oEvent){
-	    			    		oView.setBusy(false)
-	    			    	}
-	    			    	
-	    			    }
-	    	}
-	    			);
-	    	
-	    },
-		_onBindChange:function(oEvent){
-			if(!this.getView.getBindingContext())
-				this.getRouter().getTargets().display("notFound");
-		},
-		onShowResume : function (oEvent) {
-			var oCtx = this.getView().getElementBinding().getBoundContext();
+"use strict";
 
-			this.getRouter().navTo("employeeResume", {
-				employeeId : oCtx.getProperty("EmployeeID")
-			});
-		}
-	});
+return BaseController.extend("kiran.Myapp.controller.employee.Employee",{
+	onInit: function(){
+		let oRouter = this.getRouter();
+		oRouter.getRoute("employee").attachMatched(this._onRouteMatched,this);
+	},
+    _onRouteMatched:function(oEvent){
+    	var oView,oArgs;
+    	
+    	oArgs = oEvent.getParameter("arguments");
+    	oView = this.getView();
+    	
+    	oView.bindElement( {
+    				path: '/Employees(' + oArgs.employeeId + ')' ,
+    			    event: {
+    			    	change: this._onBindChange.bind(this),
+    			    	dataRequested:function(oEvent){
+    			    		oView.setBusy(true)
+    			    	},
+    			    	dataRecivied: function(oEvent){
+    			    		oView.setBusy(false)
+    			    	}
+    			    	
+    			    }
+    	}
+    			);
+    	
+    },
+	_onBindChange:function(oEvent){
+		if(!this.getView.getBindingContext())
+			this.getRouter().getTargets().display("notFound");
+	},
+	onShowResume : function (oEvent) {
+		var oCtx = this.getView().getElementBinding().getBoundContext();
+
+		this.getRouter().navTo("employeeResume", {
+			employeeId : oCtx.getProperty("EmployeeID")
+		});
+	}
+});
 });
